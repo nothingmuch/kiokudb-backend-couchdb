4de3ebacca15e224eaef93f10b0156434e2dcfab
Michael Zedeler <michael@zedeler.dk>
2010-08-30 11:18:29 +0200

    Fixing reference instantiation bug.

--- lib/KiokuX/CouchDB/Role/View.pm
+++ lib/KiokuX/CouchDB/Role/View.pm
@@ -1,15 +1,41 @@
-package KiokuX::Role::CouchDB::View;
+package KiokuX::CouchDB::Role::View;
 
 use Moose::Role;
+use Data::Dmap;
+use Carp 'croak';
+use Scalar::Util 'blessed';
 
 use namespace::clean -except => 'meta';
 
 # view() is a non-compliant method that can be called directly to query 
 # couchdb views and have all KiokuDB objects instantiated when needed
-
+# $name is a name of a CouchDB view that can contain complete KiokuDB
+# entries or references (in serialized form, of course).
 sub view {
     my($self, $name, $options) = @_;
-    return $self->deserialize($self->db->view($name, $options)->recv);
+
+    warn "Scope 1: ", $self->live_objects->current_scope, "\n";
+
+    my($result) = dmap {
+        if(ref eq 'HASH') {
+            if($_->{key} and $_->{value} and blessed $_->{value}) {
+                $_ = $self->linker->expand_object($_->{value});
+            }
+        } elsif(blessed $_ and $_->isa('KiokuDB::Reference')) {
+            my $ref_obj = $_;
+            $_ = \{'Unlinked reference' => $_};
+            $self->linker->queue_ref($ref_obj, $_);
+        }
+        $_
+    } $self->backend->deserialize($self->backend->db->view($name, $options)->recv);
+
+    warn "Scope 2: ", $self->live_objects->current_scope, "\n";
+
+    $self->linker->load_queue;
+
+    warn "Scope 3: ", $self->live_objects->current_scope, "\n";
+    
+    return $result;
 }
 
 1;
