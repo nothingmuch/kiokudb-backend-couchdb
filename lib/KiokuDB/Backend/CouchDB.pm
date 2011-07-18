f81f483c68128f35e72e8e5b3b3503fc578bc8ba
Michael Zedeler <michael@zedeler.dk>
2011-07-18 12:02:38 +0200

    Enhanced debug output.
    Minor code cleanup.
    
    Updated documentation.

--- lib/KiokuDB/Backend/CouchDB.pm
+++ lib/KiokuDB/Backend/CouchDB.pm
@@ -13,10 +13,11 @@ use Carp 'confess';
 use Try::Tiny;
 use List::MoreUtils qw{ any all };
 use Data::Visitor::Callback;
+use Time::HiRes qw/gettimeofday tv_interval/;
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 # TODO Read revision numbers into rev field and use for later conflict resolution
 
@@ -30,6 +31,8 @@ with qw(
     KiokuDB::Backend::Role::TXN::Memory
     KiokuDB::Backend::Role::Concurrency::POSIX
 );
+
+# TODO Remove TXN::Memory or ensure that it works as it should
 
 has create => (
     isa => "Bool",
@@ -74,11 +77,6 @@ has '+deleted_field' => ( default => "_deleted" );
 
 our @couch_meta_fields = qw{ _rev _attachments _conflicts };
 
-#has _prefetch => (
-#    isa => "HashRef",
-#    is  => "ro",
-#    default => sub { +{} },
-#);
 
 sub delete {
     my ( $self, @ids_or_entries ) = @_;
@@ -138,6 +136,8 @@ sub commit_entries {
     
     my @docs;
     my $db = $self->db;
+    
+    my $start = [ gettimeofday ];
 
     foreach my $entry ( @entries ) {
         
@@ -202,19 +202,13 @@ sub commit_entries {
     foreach my $rev ( map { $_->{rev} } @$data ) {
         ( shift @docs )->{_rev} = $rev;
     }
-}
 
-# this is actually slower for some reason
-#sub prefetch {
-#    my ( $self, @uids ) = @_;
-#
-#    my $db = $self->db;
-#    my $p = $self->_prefetch;
-#
-#    foreach my $uid ( @uids ) {
-#        $p->{$uid} ||= $db->open_doc($uid);
-#    }
-#}
+    if ($ENV{KIOKU_COUCH_TRACE}){
+        my $end = [ gettimeofday ];
+        warn "[KIOKU COUCH TRACE] KiokuDB::Backend::CouchDB::commit_entries() [", tv_interval($start, $end),"s]:\n";
+        warn "[KIOKU COUCH TRACE]   ".$_->id.', ['.($_->class || '')."]\n" for @entries;
+    }
+}
 
 sub get_from_storage {
     my ( $self, @ids ) = @_;
@@ -226,6 +220,7 @@ sub get_from_storage {
     my $retry_delay = 5;
     my $data;
     my $error;
+    my $start = [ gettimeofday ];
     while(not $data and $error_count <= $max_errors) {
         $error = undef;
         try   { $data = $self->db->open_docs(\@ids)->recv }
@@ -277,6 +272,13 @@ sub get_from_storage {
     # TODO What to do with deleted entries?
     # TODO What to do with entries not found?
     
+    if ($ENV{KIOKU_COUCH_TRACE}){
+        use Data::Dump 'pp';
+        warn pp caller($_) for 0 .. 100;
+        warn "[KIOKU COUCH TRACE] KiokuDB::Backend::CouchDB::get_from_storage():\n";
+        warn "[KIOKU COUCH TRACE]   ".$_->{_id}.', ['.($_->{class} || '')."]\n" for @docs;
+    }
+    
     return map { $self->deserialize($_) } @docs;
 }
 
@@ -287,21 +289,6 @@ sub deserialize {
 
     return $self->expand_jspon(\%doc, backend_data => $doc );
 }
-
-## This method takes a raw perl data structure received from the backend and deflates
-## any object references found there.
-#sub deflate_struct {
-#    my $self = shift;
-#    my $visitor = Data::Visitor::Callback->new(
-#        hash => sub {
-#            if(ref eq 'HASH' and exists $_->{'$ref'} and $_->{'$ref'} =~ /^(.+).data$/) {
-#                $_ = $self->deserialize($_);
-#            }
-#            return $_;
-#        }
-#    );
-#    return map {$visitor->visit($_)} @_;
-#}
 
 sub clear {
     my $self = shift;
@@ -352,17 +339,14 @@ KiokuDB::Backend::CouchDB - CouchDB backend for L<KiokuDB>
 
 This backend provides L<KiokuDB> support for CouchDB using L<AnyEvent::CouchDB>.
 
-Note that this is the slowest backend of all for reading data, due to the
-latency in communicating with CouchDB over HTTP.
+=head1 DEBUGGING
+
+Set the environment variable KIOKU_COUCH_TRACE if you want debug output
+describing what CouchDB bound requests are being processed.
 
 =head1 TRANSACTION SUPPORT
 
-Since CouchDB supports atomicity by using optimistic concurrency locking
-transactions are be implemented by deferring all operations until the final
-commit.
-
-This means transactions are memory bound so if you are inserting or modifying
-lots of data it might be wise to break it down to smaller transactions.
+This backend does not currently support transactions.
 
 =head1 ATTRIBUTES
 
@@ -396,7 +380,8 @@ Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
 
 =head1 CONTRIBUTORS
 
-Michael Zedeler E<lt>michael@zedeler.dk<gt>, Anders Bruun Borch E<lt>cyborch@deck.dk<gt>.
+Michael Zedeler E<lt>michael@zedeler.dk<gt>, Anders Bruun Borch E<lt>cyborch@deck.dk<gt>,
+Martin Parm E<lt>parmus@parmus.dk<gt>.
 
 =head1 COPYRIGHT
 
